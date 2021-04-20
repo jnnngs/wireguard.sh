bbf14149e3ab6b430f2313604ac5b8463e27a7ee
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-04-20 19:03:44 +0100

    123

--- index.js
+++ index.js
@@ -112,5 +112,5 @@ var clipboardDemos=new ClipboardJS('[data-clipboard-copy]');clipboardDemos.on('s
 function clearTooltip(elem){elem.setAttribute('data-tooltip','Click to Copy');}
 function showTooltip(elem,msg){elem.setAttribute('data-tooltip',msg);}
 
-var wget=new ClipboardJS('#wget');wget.addEventListener('mouseleave',clearTooltip);wget.addEventListener('blur',clearTooltip);
-var wget=new ClipboardJS('#curl');wget.addEventListener('mouseleave',clearTooltip);wget.addEventListener('blur',clearTooltip);
+var wget=new document.getElementById('wget');wget.addEventListener('mouseleave',clearTooltip);wget.addEventListener('blur',clearTooltip);
+var wget=new document.getElementById('curl');wget.addEventListener('mouseleave',clearTooltip);wget.addEventListener('blur',clearTooltip);
