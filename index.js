9d663ffdc7212d912e31a9f87d55845e2e37f51b
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-04-20 20:05:54 +0100

    .effect( "shake" );

--- index.js
+++ index.js
@@ -111,4 +111,4 @@ function t() {
 var clipboardDemos=new ClipboardJS('[data-clipboard-copy]');clipboardDemos.on('success',function(e){e.clearSelection();console.info('Action:',e.action);console.info('Text:',e.text);console.info('Trigger:',e.trigger);flash(e.trigger);showTooltip(e.trigger,'Copied!');});clipboardDemos.on('error',function(e){console.error('Action:',e.action);console.error('Trigger:',e.trigger);showTooltip(e.trigger,fallbackMessage(e.action));});
 function clearTooltip(elem){elem.setAttribute('data-tooltip','Click to Copy');}
 function showTooltip(elem,msg){elem.setAttribute('data-tooltip',msg);}
-function flash(elem) { $(elem).delay(100).fadeOut().fadeIn('slow') }
+function flash(elem) { $(elem).effect( "shake" ); }
