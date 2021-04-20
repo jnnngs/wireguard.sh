795c4dc52d84b8a0df235ec8a66eae57efff5681
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-04-20 21:39:16 +0100

    fix

--- index.js
+++ index.js
@@ -111,4 +111,7 @@ function t() {
 var clipboardDemos=new ClipboardJS('[data-clipboard-copy]');clipboardDemos.on('success',function(e){e.clearSelection();showTooltip(e.trigger,'Copied!');});clipboardDemos.on('error',function(e){console.error('Action:',e.action);console.error('Trigger:',e.trigger);showTooltip(e.trigger,fallbackMessage(e.action));});
 function clearTooltip(elem){elem.setAttribute('data-tooltip','Click to Copy');}
 function showTooltip(elem,msg){elem.setAttribute('data-tooltip',msg);}
-
+function fallbackMessage(action){var actionMsg='';var actionKey=(action==='cut'?'X':'C');if(/iPhone|iPad/i.test(navigator.userAgent)){actionMsg='No support :(';}
+else if(/Mac/i.test(navigator.userAgent)){actionMsg='Press ⌘-'+actionKey+' to '+action;}
+else{actionMsg='Press Ctrl-'+actionKey+' to '+action;}
+return actionMsg;}
