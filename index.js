de5d7cc01bb63feb5f5c8cdd7e8f40c6b1eae812
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-04-20 18:48:11 +0100

    123

--- index.js
+++ index.js
@@ -109,10 +109,6 @@ function t() {
 }
 
 var clipboardDemos=new ClipboardJS('[data-clipboard-copy]');clipboardDemos.on('success',function(e){e.clearSelection();console.info('Action:',e.action);console.info('Text:',e.text);console.info('Trigger:',e.trigger);showTooltip(e.trigger,'Copied!');});clipboardDemos.on('error',function(e){console.error('Action:',e.action);console.error('Trigger:',e.trigger);showTooltip(e.trigger,fallbackMessage(e.action));});
-var btns=document.querySelectorAll('.btn');for(var i=0;i<btns.length;i++){btns[i].addEventListener('mouseleave',clearTooltip);btns[i].addEventListener('blur',clearTooltip);}
 function clearTooltip(elem){elem.setAttribute('class','btn');elem.setAttribute('data-tooltip','Click to Copy');}
 function showTooltip(elem,msg){elem.setAttribute('class','btn');elem.setAttribute('data-tooltip',msg);}
-function fallbackMessage(action){var actionMsg='';var actionKey=(action==='cut'?'X':'C');if(/iPhone|iPad/i.test(navigator.userAgent)){actionMsg='No support :(';}
-else if(/Mac/i.test(navigator.userAgent)){actionMsg='Press ⌘-'+actionKey+' to '+action;}
-else{actionMsg='Press Ctrl-'+actionKey+' to '+action;}
-return actionMsg;}
+
