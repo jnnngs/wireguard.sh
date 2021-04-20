d860be32cea88157d4d0b83a59a1764e33a067e6
Jnnngs <4107228+jnnngs@users.noreply.github.com>
2021-04-20 18:27:11 +0100

    123

--- index.js
+++ index.js
@@ -108,16 +108,6 @@ function t() {
   }
 }
 
-function sleep( millisecondsToWait )
-{
-  var now = new Date().getTime();
-  while ( new Date().getTime() < now + millisecondsToWait )
-    {
-    /* do nothing; this will exit once it reaches the time limit */
-    /* if you want you could do something and exit */
-    }
-}
-
 var clipboardDemos=new ClipboardJS('[data-clipboard-copy]');clipboardDemos.on('success',function(e){e.clearSelection();showTooltip(e.trigger,'Copied!');});clipboardDemos.on('error',function(e){console.error('Action:',e.action);console.error('Trigger:',e.trigger);showTooltip(e.trigger,fallbackMessage(e.action));});
 var btns=document.querySelectorAll('.btn');for(var i=0;i<btns.length;i++){btns[i].addEventListener('mouseleave',clearTooltip);btns[i].addEventListener('blur',clearTooltip);}
 function clearTooltip(e){elem.setAttribute('class','btn');elem.setAttribute('data-tooltip','Click to Copy');}
